f377584957063fc341931e36b16cdc9cde7ed059
Saidul147 <sidul.islam.146@gmail.com>
2025-11-17 12:42:46 +0600

    express raw metod intrigated

--- ExpressMethods.js
+++ ExpressMethods.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
+app.use(express.raw())
 
 app.get('/',(req,res) => {
     console.log(req.body,"response from body")
@@ -13,3 +13,8 @@ app.get('/',(req,res) => {
 app.listen(4000,() => {
      console.log("server listen on port 4000")
 })
+
+
+// methods description =====================
+// express.json() - if client sent the data as json and set the headers Content-Type:application/json then it will work
+// express.raw() - if client sent the raw file that will recive here as a buffer, for this set the headers Content-Type: application/octet-strem
